37a3f63498bf7cc9b32aceaeca438794ae7feee3
Alex Kotovsky <alexktvsky@gmail.com>
2024-01-19 12:35:38 +0100

    Add changes

--- filename_sanitizer/__main__.py
+++ filename_sanitizer/__main__.py
@@ -17,20 +17,22 @@ def read_text_file(filename):
         return file.read()
 
 
-def sanitize_filename(filename, separator, table=None):
-
+def sanitize_filename(filename, separator, to_lower, table=None):
+    filename = filename.replace('\'', '')
     cmd = 'echo \'{}\''.format(filename)
-    cmd += ' | tr -d \'\\n\\r\'' # Remove new lines and tabs
+    cmd += ' | tr -d \'\\n\\r\''  # Remove new lines and tabs
+    cmd += ' | tr -d \',()\''  # Remove columns, scopes
 
     if table:
         cmd += ' | sed -e \'{}\''.format(table)
 
-    cmd += ' | tr \' \' \'{}\''.format(separator)
+    cmd += ' | tr \' \' \'{}\''.format(separator)  # Replace spaces
     cmd += ' | tr -cd \'[:alnum:]_.-\''
-    cmd += ' | tr \'[:upper:]\' \'[:lower:]\''
+
+    if to_lower:
+        cmd += ' | tr \'[:upper:]\' \'[:lower:]\''
 
     return os.popen(cmd).read()
-
 
 
 def print_verbose_info(old_name: str, new_name: str) -> None:
@@ -61,6 +63,7 @@ def rename_file(
 def rename_files_recursive(
     dirname,
     separator,
+    to_lower,
     table=None,
     interactively=False,
     verbose=False
@@ -68,19 +71,14 @@ def rename_files_recursive(
     for root, dirs, files in os.walk(dirname, topdown=False):
         for name in files:
             old_name = os.path.join(root, name)
-            new_name = sanitize_filename(name, separator, table)
             new_name = os.path.join(
-                root,
-                sanitize_filename(name, separator, table)
-            )
+                root, sanitize_filename(name, separator, to_lower, table))
             rename_file(old_name, new_name, interactively, verbose)
 
         for name in dirs:
             old_name = os.path.join(root, name)
             new_name = os.path.join(
-                root,
-                sanitize_filename(name, separator, table)
-            )
+                root, sanitize_filename(name, separator, to_lower, table))
             rename_file(old_name, new_name, interactively, verbose)
 
 
@@ -89,13 +87,13 @@ def get_data_files_path():
     return data_files_path
 
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
     parser.add_argument('-s', '--separator', type=str, default='_', help='word separator')  # noqa: E501
     parser.add_argument('-t', '--table', type=str, required=False, help='table with replacement strings')  # noqa: E501
     parser.add_argument('-f', '--file', action='store_true', default=False, help='parameter is a name of file or directory')  # noqa: E501
+    parser.add_argument('-l', '--lower', action='store_true', default=False, help='transform text to lowercase')  # noqa: E501
     parser.add_argument('-r', '--recursive', action='store_true', default=False, help='sanitize directories and their contents recursively')  # noqa: E501
     parser.add_argument('-i', '--interactively', action='store_true', default=False, help='prompt before rename')  # noqa: E501
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='explain what is being done')  # noqa: E501
@@ -110,26 +108,26 @@ def main():
         table = read_text_file(table_filename)
 
     if not args.file:
-        print(sanitize_filename(args.filename, args.separator, table))
+        new_name = sanitize_filename(
+            args.filename, args.separator, args.lower, table)
+        print(new_name)
         return 0
 
     if args.recursive:
         rename_files_recursive(
             args.filename,
             args.separator,
+            args.lower,
             table,
             args.interactively,
             args.verbose
         )
-        return 0
     else:
-        rename_file(
-            args.filename,
-            sanitize_filename(old_name, args.separator, args.table),
-            args.interactively,
-            args.verbose
-        )
-        return 0
+        new_name = sanitize_filename(
+            args.filename, args.separator, args.lower, table)
+        rename_file(args.filename, new_name, args.interactively, args.verbose)
+
+    return 0
 
 
 if __name__ == '__main__':
